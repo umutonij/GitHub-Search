376bf4d367d7b668c47f8b00705118f05a8688cc
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-04 15:55:23 +0200

    add the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+// import { ProfileComponent } from './profile/profile.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
+    // ProfileComponent,
     ProfileFormComponent
   ],
   imports: [
